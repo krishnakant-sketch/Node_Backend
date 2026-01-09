f843c2a20aab1ec6a42734a259839819c2b650fe
krishnakant-sketch <krishnakant@angularminds.com>
2026-01-09 14:42:02 +0530

    password change

--- mern-auth/server.js
+++ mern-auth/server.js
@@ -18,7 +18,7 @@ app.use("/webhook/razorpay", bodyParser.raw({ type: "application/json" }));
 
 mongoose
   .connect(
-    "mongodb+srv://krishnakant_db_user:<password>@cluster0.ttpqlds.mongodb.net/mernAuth?retryWrites=true&w=majority"
+    "mongodb+srv://krishnakant_db_user:Krishna%4010@cluster0.ttpqlds.mongodb.net/mernAuth?retryWrites=true&w=majority"
   )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
